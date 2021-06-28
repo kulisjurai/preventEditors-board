2f52a9f4c86c29673355a8817568dbda11b0915f
jurakulis21 <71762114+jurakulis21@users.noreply.github.com>
2021-06-28 10:59:07 +0200

    description

--- editors-board/client/src/components/header/Header.jsx
+++ editors-board/client/src/components/header/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
     <div className="header">
       <div className="headerTitles">
         <span className="headerTitleSm">
-          Sve o bh planinama, planinarskoj opremi, hrani i ljekovitom bilju{" "}
+          Sve o bh planinama, planinarskim rutam i planinarskoj opremi
         </span>
       </div>
       <img
